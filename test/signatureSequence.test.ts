6c1fd49e00ca4c4ae6863449039695f395dcbdec
hannahellis4242 <hannahellis4242@gmail.com>
2023-06-30 22:54:23 +0100

    convert test to parameterised test

--- test/signatureSequence.test.ts
+++ test/signatureSequence.test.ts
@@ -1,10 +1,8 @@
 const signatureSequence = (_: number) => [4, 1, 2, 3];
 
 describe("signatureSequence", () => {
-  describe("when I have a signature of 1 sheet", () => {
-    it("then signatureSequence should give back [4,1,2,3]", () => {
-      const result = signatureSequence(1);
-      expect(result).toStrictEqual([4, 1, 2, 3]);
-    });
-  });
+  test.each([[1, [4, 1, 2, 3]]])(
+    "when I have a signature of size %i and I call signatureSequence, it should return %i",
+    (size, expected) => expect(signatureSequence(size)).toStrictEqual(expected)
+  );
 });
