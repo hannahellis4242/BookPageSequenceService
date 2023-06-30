91141dd916d6605337cd5b2fd3b3b0b086dffd55
hannahellis4242 <hannahellis4242@gmail.com>
2023-06-30 23:17:13 +0100

    Adding even more tests cases, covering the most likely signature sizes

--- test/signatureSequence.test.ts
+++ test/signatureSequence.test.ts
@@ -1,7 +1,37 @@
-const signatureSequence = (_: number) => [4, 1, 2, 3];
+function* signatureSequenceGenerator(sheets: number) {
+  const pages = 4 * sheets;
+  {
+    let index = 0;
+    let next = pages;
+    while (index < pages) {
+      yield next;
+      const step = index % 4;
+      if (step % 2 == 0) {
+        next = pages + 1 - next;
+      } else if (step % 4 == 1) {
+        ++next;
+      } else if (step % 4 == 3) {
+        --next;
+      }
+      ++index;
+    }
+  }
+}
+const signatureSequence = (sheets: number) => [
+  ...signatureSequenceGenerator(sheets),
+];
 
 describe("signatureSequence", () => {
-  test.each([[1, [4, 1, 2, 3]]])(
+  test.each([
+    [1, [4, 1, 2, 3]],
+    [2, [8, 1, 2, 7, 6, 3, 4, 5]],
+    [3, [12, 1, 2, 11, 10, 3, 4, 9, 8, 5, 6, 7]],
+    [4, [16, 1, 2, 15, 14, 3, 4, 13, 12, 5, 6, 11, 10, 7, 8, 9]],
+    [
+      5,
+      [20, 1, 2, 19, 18, 3, 4, 17, 16, 5, 6, 15, 14, 7, 8, 13, 12, 9, 10, 11],
+    ],
+  ])(
     "when I have a signature of size %i and I call signatureSequence, it should return %i",
     (size, expected) => expect(signatureSequence(size)).toStrictEqual(expected)
   );
